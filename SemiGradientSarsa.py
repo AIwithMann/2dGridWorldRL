583bdb20cd6e573b089b054a76615693b7208106
Mann Makwana <aiwithmann@gmail.com>
2025-10-23 10:12:32 +0530

    Update SemiGradientSarsa.py

--- SemiGradientSarsa.py
+++ SemiGradientSarsa.py
@@ -12,9 +12,6 @@ class SemiGradientSarsa:
         self.maxIter = maxIterations
         self.delta = delta
 
-        self.pos = np.zeros(2)
-        self.action = -1
-        self.features = np.zeros(self.d)
         self.w = np.zeros(self.d)
 
         self.actions={
@@ -29,87 +26,51 @@ class SemiGradientSarsa:
         # Then we won't need tile coding or other such methods
 
     def Q(self,pos:np.ndarray=None,action=None)->float:
-        if pos is not None and action is not None:
-            return np.dot(self.w,np.array([pos[0],pos[1],action]))
-        return np.dot(self.w,self.features)
+        return np.dot(self.w,np.array([pos[0],pos[1],action]))
 
 #--------------------------------------------------------------------------------------------------------------------------------
-    def actL(self,pos:np.ndarray=None,plan:bool=False):
-        y = self.pos[1]-1
-        if y<0 and plan:
-            return np.array([self.pos[0],0])
-        elif y<0 and not plan:
-            self.features[1] = 0
-        elif y>=0 and not plan:
-            return np.array([self.pos[0],y])
-        else:
-            self.features[1] = y
-
-    def actU(self,pos:np.ndarray=None,plan:bool=False):
-        x = self.pos[0]-1
-        if x<0 and plan:
-            return np.array([0,self.pos[1]])
-        elif x<0 and not plan:
-            self.features[0] = 0
-        elif x>=0 and plan:
-            return np.array([x,self.pos[1]])
-        else:
-            self.features[0] = x
-
-    def actR(self,pos:np.ndarray=None,plan:bool=False):
-        y = self.features[1]+self.delta
-        if y>=self.env.yInterval and plan:
-            return np.array([self.pos[0],self.env.yInterval-(10e-5)])
-        elif y>=self.env.yInterval and not plan:
-            self.features[1] = self.env.yInterval-(10e-5)
-        elif y<self.env.yInterval and plan:
-            return np.array([self.pos[0],y])
-        else:
-            self.features[1] = y
-
-    def actD(self,pos:np.ndarray=None,plan:bool=False):
-        x = self.features[0]+self.delta
-        if x>=self.env.xInterval and plan:
-            return np.array([self.env.xInterval-(10e-5),self.pos[1]])
-        elif x>=self.env.xInterval and not plan:
-            self.features[0] = self.env.xInterval-(10e-5)
-        elif x<self.env.xInterval and plan:
-            return np.array([x,self.features[1]])
-        else:
-            self.features[0] = x
+    def actL(self, pos): return np.array([pos[0], max(0, pos[1] - self.delta)])
+    def actU(self, pos): return np.array([max(0, pos[0] - self.delta), pos[1]])
+    def actR(self, pos): return np.array([pos[0], min(self.env.yInterval - 1e-5, pos[1] + self.delta)])
+    def actD(self, pos): return np.array([min(self.env.xInterval - 1e-5, pos[0] + self.delta), pos[1]])
             
 #--------------------------------------------------------------------------------------------------------------------------------
 
-    def highestValueAction(self):
-        actionValues = [self.Q(f(plan=True)) for f in [self.actL,self.actU,self.actR,self.actD]]
-        return max(enumerate(actionValues),key= lambda x:x[1])[0]
+    def highestValueAction(self,pos:np.ndarray):
+        actionValues = [self.Q(self.actions[a](pos), a) for a in range(4)]
+        return max(range(4), key=lambda a: actionValues[a])
 
-    def act(self):
+    def act(self,pos):
         prob = r.random()
         if prob > self.e:
-            action = self.highestValueAction()
-            self.actions[action]()
+            action = self.highestValueAction(pos)
         else:
             action = r.choice([0,1,2,3])
-            self.actions[action]()
-        return action
+        newPos = self.actions[action](pos)
+        return action, newPos
 
     def learn(self):
         for episode in range(self.maxIter):
-            self.pos = np.array([r.uniform(a=0,b=self.env.xInterval),r.uniform(a=0,b=self.env.yInterval)])
-            a = self.act()
-            self.features[:] = self.pos[0], self.pos[1], a
+            print(episode, end='\n')
+            pos = np.array([r.uniform(a=0,b=self.env.xInterval),r.uniform(a=0,b=self.env.yInterval)])
+            a,nextS = self.act(pos)
             
             while True:
-                if any((np.round(self.features[0]) == np.round(xs)) and (np.round(self.features[1]) == np.round(ys)) for xs, ys in self.env.terminationStates): 
-                    self.w += self.alpha * (self.env.reward(self.features[0],self.features[1]) - self.Q()) * self.features
+                reward = self.env.reward(pos[0],pos[1])
+                phi = np.array([pos[0],pos[1],a])
+                if any(np.allclose(pos, t) for t in self.env.terminationStates):
+                    self.w += self.alpha * (reward - self.Q(pos,a)) * phi
                     break 
-                nextA = self.act()
-                self.w += self.alpha * (self.env.reward(self.features[0],self.features[1]) + self.g *self.Q(self.pos,nextA) - self.Q())*self.features
-                self.features[:] = self.pos[0],self.pos[1],nextA
+
+                nextA,nextNextS = self.act(nextS)
+                target = reward + self.g*self.Q(nextS,nextA)
+                self.w += self.alpha * (target- self.Q(pos,a))*phi
+
+                pos, a, nextS = nextS, nextA, nextNextS
+
             print(episode,"\n")
 #driver code
-env = ENV(10,10,4)
-agent = SemiGradientSarsa(env,0.3,0.9,0.25,1000,1)
+env = ENV(10,10,10)
+agent = SemiGradientSarsa(env,0.01,0.9,0.25,1000,1.1)
 agent.learn()
                     
